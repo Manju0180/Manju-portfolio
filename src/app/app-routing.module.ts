2263cfad017fa7d9f107c9cbd6f4c3a36523bfbb
Manju0180 <manjunathkalapur474@gmail.com>
2024-09-25 23:53:35 +0530

    Added-other-components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponentComponent } from './components/home-component/home-component.component';
 import { AboutComponentComponent } from './components/about-component/about-component.component';
+import { ResumeComponent } from './components/resume/resume.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 const routes: Routes = [
   {path:'', redirectTo:'/home', pathMatch:'full'},
   {path:'home', component:HomeComponentComponent},
   {path:'about',component:AboutComponentComponent},
+  {path: 'resume', component:ResumeComponent},
+  {path:'projects', component:ProjectsComponent},
+  {path:'contact', component:ContactComponent},
   { path: '**', redirectTo: '/home' }
 ];
 
